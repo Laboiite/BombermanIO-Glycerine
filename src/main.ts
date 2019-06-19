eec74eb6daea01990cb7dfb603619e7364a47ffd
HAUSPIE Gael <gael.hauspie@soprabanking.com>
2019-06-19 14:34:02 +0200

    Add our first gateway, receiving a connection and returning data

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { WsAdapter } from "@nestjs/platform-ws";
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule, {
+		logger: console,
+  });
+	app.useWebSocketAdapter(new WsAdapter(app));
+	await app.listen(3000);
 
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+	if (module.hot) {
+		module.hot.accept();
+		module.hot.dispose(() => app.close());
+	}
 }
 bootstrap();
